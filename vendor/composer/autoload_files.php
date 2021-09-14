e025612b3244e9b0904e3fcdb42d827ef202bbd4
Md Rakibul Islam <irakibul189@gmail.com>
2021-09-14 12:22:53 +0600

    update codestar

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '968a1f4a95458126e7dbfd0bfdf769b4' => $baseDir . '/includes/framework/codestar-framework.php',
+    '7681fa2c026a80897e746efc069d5879' => $baseDir . '/includes/codestar-framework/codestar-framework.php',
     '1c01d6ef906adbf591e12a4a73198e88' => $baseDir . '/helpers/helpers.php',
     '78efd2ad2e874d7c30f73ea58053fbec' => $baseDir . '/helpers/admin-options.php',
     '2ed3697b184f77c8f16bbf7b6adfe604' => $baseDir . '/includes/builder-functions.php',
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,7 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInitc8ab8da1e405711f9828950eadd54b65
 {
     public static $files = array (
-        '968a1f4a95458126e7dbfd0bfdf769b4' => __DIR__ . '/../..' . '/includes/framework/codestar-framework.php',
+        '7681fa2c026a80897e746efc069d5879' => __DIR__ . '/../..' . '/includes/codestar-framework/codestar-framework.php',
         '1c01d6ef906adbf591e12a4a73198e88' => __DIR__ . '/../..' . '/helpers/helpers.php',
         '78efd2ad2e874d7c30f73ea58053fbec' => __DIR__ . '/../..' . '/helpers/admin-options.php',
         '2ed3697b184f77c8f16bbf7b6adfe604' => __DIR__ . '/../..' . '/includes/builder-functions.php',
